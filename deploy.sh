c88ba665b2ffc8bef52f2999d4dd9f260febe6d9
Pieter Malan <pjdvmalan@hotmail.com>
2026-03-06 13:32:37 +0200

    Deploy script force git clear and install node packages.

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,16 @@ set -e
 
 echo "=== Kiosk Deploy ==="
 
-echo ">> Navigating to ~/src/kiosk..."
-cd ~/src/kiosk
+# echo ">> Navigating to ~/src/kiosk..."
+# cd ~/src/kiosk
 
-echo ">> Pulling latest code..."
-git pull
+echo ">> Pulling latest code (overriding local changes)..."
+git fetch origin
+git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)
+git clean -fd
+
+echo ">> Installing dependencies..."
+npm install
 
 echo ">> Building application..."
 npm run build
